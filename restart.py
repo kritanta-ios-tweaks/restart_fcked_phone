94e05ef0aca3185ecfaba491168bfad07a0eed41
Kritanta <55725881+KritantaDev@users.noreply.github.com>
2020-01-06 04:12:18 -0600

    Fix

--- restart.py
+++ restart.py
@@ -1,8 +1,11 @@
 from pymobiledevice.diagnostics_relay import DIAGClient
 
 x = DIAGClient()
+
 try:
     x.restart()
     # this will throw an error, but it should work :)
-    
-print("Request sent, should be restarting now :)")
+except Exception:
+    pass
+
+print ('Request sent, should be restarting now :)')
